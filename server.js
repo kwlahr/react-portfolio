36d300d86367af0ec2a59919ad3a017fa3354ded
kwlahr <kwlahr@gmail.com>
2020-03-27 14:28:26 -0600

    updated server for github pages

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const routes = require("./routes");
+
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,8 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
-
+// app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
