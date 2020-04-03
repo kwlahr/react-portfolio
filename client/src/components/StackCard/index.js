d1000b48b1413e9698a9e48ff51cd3d7429f0f8c
kwlahr <kwlahr@gmail.com>
2020-04-02 20:25:35 -0600

    fixed StackCard styling

--- client/src/components/StackCard/index.js
+++ client/src/components/StackCard/index.js
@@ -17,15 +17,16 @@ const StackCard = () => {
         <Card
           color="dark"
           className="mx-auto"
-          style={{ padding: "1rem", border: "solid 2px ", marginTop: "2rem" }}
+          style={{ padding: "1rem", border: "solid 2px ", marginTop: "2rem", textAlign: "center" }}
         >
           <Row>
             <CardBody>
               <CardTitle className="text-white font-weight-bold">
                 <h1>Technical Skills</h1>
               </CardTitle>
-              <CardSubtitle className="text-white" style={{textAlign: "center"}}>MERN Focus</CardSubtitle>
-              <CardText className="mt-3 text-white" style={{textAlign: "center"}}>
+              <br/>
+              <CardSubtitle className="text-white">MERN Focus</CardSubtitle>
+              <CardText className="mt-3 text-white">
                 HTML5 | CSS | Bootstrap | Materialize | Javascript | React | JQuery |
                 Node.js | Express | Express-Handlebars | JawsDB | AWS | MySQL |
                 Sequelize | MongoDB | Mongoose | GitHub | Postman | Heroku |
