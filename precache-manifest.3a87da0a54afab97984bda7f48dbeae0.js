f69269e5416954daaa200c56f61ccd92f97ea0e5
kwlahr <kwlahr@gmail.com>
2021-01-13 19:00:34 -0700

    Updates

---
+++ precache-manifest.3a87da0a54afab97984bda7f48dbeae0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638fd4455d51b80222489ed675a416d8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ca20eccf61eb4c743f6a",
+    "url": "/react-portfolio/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "d5b62d926efe2a25c393",
+    "url": "/react-portfolio/static/css/main.5cb4ba01.chunk.css"
+  },
+  {
+    "revision": "ca20eccf61eb4c743f6a",
+    "url": "/react-portfolio/static/js/2.7d8c32af.chunk.js"
+  },
+  {
+    "revision": "252217825bb687eb0601a9c3a18ad6fd",
+    "url": "/react-portfolio/static/js/2.7d8c32af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b62d926efe2a25c393",
+    "url": "/react-portfolio/static/js/main.539cf303.chunk.js"
+  },
+  {
+    "revision": "6c1a97d7eedcd4b749e8",
+    "url": "/react-portfolio/static/js/runtime-main.fcfd7c4a.js"
+  },
+  {
+    "revision": "23f7e21c007e63c6fff286641640df3d",
+    "url": "/react-portfolio/static/media/burgerApp.23f7e21c.gif"
+  },
+  {
+    "revision": "4f250126853a3759bac7b7dd3bc0367e",
+    "url": "/react-portfolio/static/media/kyleheadshot.4f250126.jpg"
+  },
+  {
+    "revision": "0b5d7d41570f964b69d5081c31536bc7",
+    "url": "/react-portfolio/static/media/noteTaker.0b5d7d41.gif"
+  },
+  {
+    "revision": "c66cf51f04f111dd455a544ef7ec1c92",
+    "url": "/react-portfolio/static/media/react-weather-app.c66cf51f.gif"
+  },
+  {
+    "revision": "2066a72bd7f9fde81fd69c5b04387581",
+    "url": "/react-portfolio/static/media/weatherDashboard.2066a72b.gif"
+  },
+  {
+    "revision": "4dcc501917a549a93e7cdcd358fb44c7",
+    "url": "/react-portfolio/static/media/whiskeyJournal.4dcc5019.gif"
+  }
+]);
